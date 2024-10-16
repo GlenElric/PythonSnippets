8daff6c311858b42e7889b3b9255778e5620bad9
GlenElric <22j21.glen@sjec.ac.in>
2024-10-16 08:07:12 +0530

    Generalized

--- Auto-mails.py
+++ Auto-mails.py
@@ -2,53 +2,32 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-# Email credentials
-sender_email = "***********@********.com"
-sender_password = "**************" 
+sender_email = "#Your email"
+sender_password = "#Your password"
 
-# Email details
-subject = "Selection Process for NCC Naval Unit- October 14th, 2024"
-body = """Dear [Candidate Name],
-Jai Hind!
-
-We are pleased to inform you that you have been shortlisted for the selection process for the Naval NCC SJEC. The selection process will be held on **October 14th, 2024**, commencing at **2:00 PM** and concluding at **5:00 PM**.
-
-The selection process will consist of the following:
-
-**2:00 PM - 3:00 PM:** Written Test
-**3:00 PM - 5:00 PM:** Physical Ability Test
-
-Please arrive at NCC room, Opposite PED (AB3 Ground Floor) by **1:45 PM** on the day of the selection process. We advise you to stay hydrated and carry a water bottle.
-
-**Dress Code:** T-shirt (preferably collared) with track pants and sports shoes.
-
-We look forward to meeting you and wish you the best of luck.
-
-Sincerely,
-L Cdt.Glen Elric
-NCC Naval Unit, SJEC
+emails_mapping = {
+    "Recipient1": "recipient1@mail.com",
+    "Recipient2": "recipient2@mail.com",
+}
+subject = "Customized subject for each Recipient"
+body = """ Salutation {name},
+#Email tailored according to your requirement 
 
 """
 
-# List of candidate emails
-candidates = [
-    "recipient1@gmail.com"
-]
 
 # Create a secure connection with the server
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.login(sender_email, sender_password)
 
-for candidate_email in candidates:
+for candidate_name, candidate_email in emails_mapping.items():
     msg = MIMEMultipart()
     msg['From'] = sender_email
     msg['To'] = candidate_email
     msg['Subject'] = subject
 
-    # Replace placeholder with candidate name
-    personalized_body = body.replace("[Candidate Name]", candidate_email.split("@")[0])
-
     # Attach the body to the message
+    personalized_body = body.format(name=candidate_name)
     msg.attach(MIMEText(personalized_body, 'plain'))
 
     # Send the email
